0b8225362adecc37c3c0172946a14d2ad8330bea
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-03-01 22:02:02 +0530

    Create Day1.cpp

---
+++ Day1.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+        if(arr[0]>=arr[1]){
+           return 0;
+       }
+       
+       if(arr[n-1]>=arr[n-2]){
+           return n-1;
+       }
+       
+       
+       
+       
+    }
+};
+
+
+//fgjfghkj
