f84c3a7293374f6344ea2fc17a59616f8f322ce6
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-03-01 22:32:02 +0530

    Day2.cpp

---
+++ Day2.cpp
@@ -0,0 +1,22 @@
+//Solution for Sum of bit differences question in GeeksforGeeks
+class Solution{
+public:
+	
+	long long sumBitDifferences(int arr[], int n) {
+	    long long ans = 0;
+	    
+	    for(int i=0; i<32; i++){
+	        long long set = 0;
+	        long long notset = 0;
+	        
+	        for(int j=0; j<n; j++){
+	            if(((arr[j]>>i) & 1) == 1)
+	                set++;
+	            else
+	                notset++;
+	        }
+	        ans+= 2*(set*notset);
+	    }
+	    return ans;
+	}
+};
