2b58fb66ad4584b98aa844f020b720ed3c88f9e9
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-03-04 23:04:35 +0530

    Create Day3.cpp

---
+++ Day3.cpp
@@ -0,0 +1,24 @@
+//solution to "Chocolate Distribution" problem GFG
+class Solution {
+  public:
+    int chocolateDistribution(int N, int M, vector<int> &arr, vector<int> &brr) {
+        sort(arr.begin(), arr.end()); //{8,15,20,55}
+        sort(brr.begin(), brr.end()); //{2,17,22}
+        
+        int count = 0;
+        int i=0,j=0;
+        
+        while(i<N && j<M){
+            if(arr[i]<=brr[j]){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        
+        return count;
+    }
+};
