47cbb0414739a9a62d49d2bcc85d98622ed3fd95
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-03-08 00:41:49 +0530

    Contains Duplicate Day5.cpp

---
+++ Day5.cpp
@@ -0,0 +1,9 @@
+//solution of "Contains Duplicate" question LeetCode
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]) return 1;
+        }
+        return 0;
