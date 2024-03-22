54e7a479fd53af8da0e1083d60c6be5a8917e267
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-03-22 15:44:31 +0530

    Create Day15.cpp

---
+++ Day15.cpp
@@ -0,0 +1,20 @@
+//First Greater Height in GFG
+vector<int> greater_height(vector<int>v)
+{
+    vector<int>res;
+    set<int>s;
+    for(int i =0; i<v.size(); i++){
+        auto it = s.upper_bound(v[i]);
+        
+        if(it != s.end()){
+            res.push_back(*it);
+        }
+        
+        else{
+            res.push_back(-1);
+        }
+        
+        s.insert(v[i]);
+    }
+    return res;
+}
