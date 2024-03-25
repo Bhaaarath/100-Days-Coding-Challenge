06c072d590044d7f45fcddcd9a92fc080c34174d
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-03-25 21:26:50 +0530

    longest consecutive 1's Day20.cpp

---
+++ Day20.cpp
@@ -0,0 +1,29 @@
+//longest consecutive 1's GFG
+#include <bits/stdc++.h>
+string toBinary(int x){
+    string binarynum  ="";
+    while(x>0){
+        binarynum += to_string(x%2);
+        x/=2;
+    }
+    reverse(binarynum.begin(), binarynum.end());
+    return binarynum;
+}
+
+int maxConsecutiveOnes(int x)
+{
+    int largest = INT_MIN;
+    string binrep = toBinary(x);
+    for(int i=0; i<binrep.length(); i++){
+        int len = 0;
+        if(binrep[i] == '1'){
+            while(binrep[i]=='1'){
+                len++;
+                largest = max(largest, len);
+                i++;
+            }
+        }
+    }
+    
+    return largest;
+}
