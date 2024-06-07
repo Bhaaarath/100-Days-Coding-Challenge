734e917fb25f608079feb2bbf568109cfa19bb81
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-06-07 19:59:18 +0530

    Longest Subarray With Sum K.cpp

---
+++ Day30.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+int getLongestSubarray(vector<int>& a, int k){
+    int n = a.size();
+    int maxlen = 0; 
+    map<int,int>sni;
+    int currsum = 0;
+
+    for(int i=0; i<n; i++){
+        currsum+=a[i];
+        if(currsum == k){
+            maxlen = max(maxlen, i+1);
+        }
+
+        int cmk = currsum - k;
+        if(sni.find(cmk) != sni.end()){
+            int len = i- sni[cmk];
+            maxlen = max(maxlen, len);
+        }
+
+        if(sni.find(currsum) == sni.end()){
+            sni[currsum] = i;
+        }
+    }
+    return maxlen;
+}
