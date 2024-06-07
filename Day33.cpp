bdb486d5caef00ec71f4ab74c0ea8b3013591390
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-06-07 20:27:50 +0530

    Longest Subarray With Sum K.cpp

---
+++ Day33.cpp
@@ -0,0 +1,21 @@
+//Longest Subarray With Sum K (only positives, optimal solution using 2 pointers)
+#include<bits/stdc++.h>
+int longestSubarrayWithSumK(vector<int> arr, long long k) {
+    int i=0, j=0, n = arr.size();
+    long long sum = 0;
+    int len = 0;
+    while(j<n){
+        sum += arr[j];
+
+        while(sum>k){
+            sum -= arr[i];
+            i++;
+        }
+
+        if(sum == k){
+            len = max(len, j-i+1);
+        }
+        j++;
+    }
+    return len;
+}
