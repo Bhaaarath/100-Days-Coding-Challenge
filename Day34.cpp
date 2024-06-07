fa8d5b4ce0f07d870c1f20a69c172892fc9efef7
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-06-07 23:24:01 +0530

    Two Sum Problem.cpp

--- Day34.cpp
+++ Day34.cpp
@@ -7,3 +7,23 @@ string read(int n, vector<int> book, int target)
     }
     return "NO";
 }
+/*
+#include <bits/stdc++.h>
+string read(int n, vector<int> book, int target)
+{
+    sort(book.begin(), book.end());
+    int i = 0, j = n-1;
+    while(i<j){
+        if(book[i] + book[j] < target){
+            i += 1;
+        }
+        if(book[i] + book[j] > target){
+            j -= 1;
+        }
+        if(book[i] + book[j] == target){
+            return "YES";
+        }
+    }
+    return "NO";
+}
+*/
