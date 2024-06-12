a79e309fdf8cee42ac63fedcfe9f9bd35c6ed5ce
Bhaaarath <160698981+Bhaaarath@users.noreply.github.com>
2024-06-12 14:21:33 +0530

    Majority Element.cpp

---
+++ Day41.cpp
@@ -0,0 +1,20 @@
+vector<int> majorityElement(vector<int> v) {
+	int cnt = 0, n=v.size(), curele = v[0];
+	vector<int>res;
+	sort(v.begin(), v.end());
+	for(int i=0; i<n; i++){
+		if(v[i] == curele){
+			cnt+=1;
+		}
+		else{
+			cnt = 1;
+			curele = v[i];
+		}
+
+		if(cnt> (n/3) && find(res.begin(), res.end(), curele) == res.end()){
+            res.push_back(curele);    
+        }
+	}
+
+	return res;
+}
